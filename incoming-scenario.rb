366df5be08edaa423a4f456f1d4245e86cb77fac
Brenna Flood <brenna@splice.com>
2020-07-20 14:46:04 -0700

    Update: Add clear, ability to run multiple scenarios in a single run.

--- incoming-scenario.rb
+++ incoming-scenario.rb
@@ -1,4 +1,10 @@
-filename = "scenario.txt"
+require 'io/console'
+
+filename = "scenarios.txt"
+
+if ARGV[0] == nil 
+	ARGV[0] = 1
+end
 
 # read in the text file and save values as an array
 def arraycreate(filename)
@@ -9,9 +15,9 @@ end
 # select the scenario using sample method
 def selector(array, filename)
 	selected = array.sample
-	modifyfile(selected, filename)
-  puts "clear"
-  puts "Incoming Scenario: "+selected.strip
+	system "clear"
+	  puts "Incoming Scenario: "+selected.strip+"\n\n (Press any key to continue.)"
+	  STDIN.getch 	  
 end
 
-arraycreate(filename)
+ARGV[0].to_i.times { arraycreate(filename) }
